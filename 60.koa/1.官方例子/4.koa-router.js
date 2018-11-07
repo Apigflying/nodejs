fd79d2cd8cf7833a03ca1edeceee61db90018f75
Apigflying <809276596@qq.com>
2018-11-07 22:08:14 +0800

    提交数据结果

--- 60.koa/1.官方例子/4.koa-router.js
+++ 60.koa/1.官方例子/4.koa-router.js
@@ -20,20 +20,50 @@ router.get('/', (ctx, next) => {
 router.post('/signUp', async (ctx, next) => {
   const { request: req, response: res } = ctx;
   res.data = 123;
+  console.log('1同步');
   await next();
-  console.log(res.body);
+  console.log('1异步');
+  // 相同的处理函数，先走await前面的，再走后面的函数，然后走这里，最后返回给前端的数据由最后一个next()之后的res.body决定
+  res.body = { a: 123 }
 })
 
 router.post('/signUp', async (ctx, next) => {
-  var name = ctx.request.body.name || '', password = ctx.request.body.password || '';
-  console.log(ctx.response.data);
+  const { request: req, response: res } = ctx;
+  var name = req.body.name || '', password = req.body.password || '';
   if (name === 'koa' && password === '12345') {
-    ctx.response.body = `<h1>Welcome, ${ name }!</h1>`;
+    res.body = `<h1>Welcome, ${ name }!</h1>`;
   } else {
-    ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
+    res.body = `<h1>Login failed!</h1><p><a href="/">Try again</a></p>`;
+  }
+  console.log('2同步');
+  await next();
+  console.log('2异步')
+  res.body = {
+    abc: 234
   }
 })
+
+
+
+router.post('/signUp', async (ctx, next) => {
+  console.log('3同步');
+  await next();
+  console.log('3异步')
+})
+
+/*
+
+上面处理请求的函数，最后的执行结果如下
+  1同步
+  2同步
+  3同步
+  3异步
+  2异步
+  1异步
+
+  第一个异步，会在最后被处理
+  最后一个同步函数处理完之后，会优先处理最近的（最后一个）同步函数
+*/
 
 app.use(router.routes());
 
